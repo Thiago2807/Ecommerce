52a33729fb50eec1024211e8bf59f795bd3db8a9
Thiago2807 <thiagodasilva410@gmail.com>
2025-04-21 11:24:15 -0300

    fix: Configurações iniciais na API de Produtos e Categorias

---
+++ ecommerce-api/api-ecommerce-pct/Services/DependencyInjection.cs
@@ -0,0 +1,15 @@
+﻿using ecommerce_core.Exceptions;
+
+namespace api_ecommerce_pct.Services;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddDependencyInjection(this IServiceCollection services, IConfiguration configuration)
+    {
+        services.AddExceptionHandler<ExceptionHandlerCustom>();
+
+        //services.AddSingleton<IExceptionHandler, ExceptionHandlerCustom>();
+
+        return services;
+    }
+}
